3b26b23e78b9cbc473bee2cd9f375edd7d6b8c20
tomatosugarpickled <tjdgh1851@gmail.com>
2026-03-19 11:21:52 +0900

    강사님 코드 복사

---
+++ src/main/java/com/app/globalgates/common/exception/MemberLoginFailException.java
@@ -0,0 +1,6 @@
+package com.app.globalgates.common.exception;
+
+public class MemberLoginFailException extends RuntimeException{
+    public MemberLoginFailException() {}
+    public MemberLoginFailException(String message) {super(message);}
+}
